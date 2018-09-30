4971fa337208c2ee258f3fc64a5e936e498b2f41
Thai Son Dang <thaisondang.bk57@gmail.com>
2018-09-30 14:05:38 +0200

    overlay of Menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('compile-sass', () => {
     gulp.src('./src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('./dist/css'))
 });
 
